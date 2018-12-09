93f4d51da805d4a6d20f5e29fb6968df1972b34e
Michael Bledsoe <admin@thinkly.com>
2018-12-08 19:39:01 -0500

    Halfway through Chapter 3 Little Schemer

---
+++ LittleSchemer/03-Cons-The-Magnificent.rkt
@@ -0,0 +1,59 @@
+#lang racket
+
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
+
+(define rember
+  (lambda (a l)
+    (cond
+      ((null? l) '())
+      ((eq? a (car l)) (cdr l))
+      (else (cons (car l) (rember a (cdr l)))))))
+; I wrote the previous function as an exercise before continuing, so working through
+; understanding the need for the cons piece of this function is omitted.
+; It's necessary to cons the (car l) onto the result of the recursion so then
+; the original list is rebuilt on the way out of the call stack.6
+
+(rember 'mint '(lamb chops and mint jelly))
+    
+(rember 'bacon '(bacon lettuce and tomato))
+
+(rember 'and '(bacon lettuce and tomato))
+
+(rember 'sauce '(soy sauce and tomato sauce))
+
+(define firsts
+  (lambda (l)
+    (cond
+      ((null? l) '())
+      (else (cons (car (car l)) (firsts (cdr l)))))))
+
+(firsts
+ '(
+   (apple peach pumpkin)
+   (plum pear cherry)
+   (grape raisin pea)
+   (bean carrot eggplant)))
+
+(firsts
+ '((a b) (c d) (e f)))
+
+; The Third Commandment
+; When building a list, describe teh first typical element, and
+; then cons it onto the natural recursion.
+
+
+
+
+
+
+
+
