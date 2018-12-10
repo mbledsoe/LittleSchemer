99e08f2b464067f156aea27a9aec84b0c7e1d88c
Michael Bledsoe <admin@thinkly.com>
2018-12-09 22:09:05 -0500

    Halfway through LittleSchemer Chapter 5

---
+++ LittleSchemer/05-Oh-My-Gawd-Its-Full-of-Stars.rkt
@@ -0,0 +1,87 @@
+#lang racket
+
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define rember*
+  (lambda (a l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) a) (rember* a (cdr l)))
+         (else (cons (car l) (rember* a (cdr l))))))
+      (else (cons (rember* a (car l)) (rember* a (cdr l)))))))
+
+(rember* 'cup '((coffee) cup ((tea) cup) (and (hick)) cup))
+
+(rember* 'sauce '(((tomato sauce) ((bean) sauce) (and ((flying) sauce)))))
+         
+(define insertR*
+  (lambda (new old l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) old) (cons old (cons new (insertR* new old (cdr l)))))
+         (else (cons (car l) (insertR* new old (cdr l))))))
+      (else (cons (insertR* new old (car l)) (insertR* new old (cdr l)))))))
+
+(insertR* 'roast 'chuck '((how much (wood)) could ((a (wood) chuck)) (((chuck))) (if (a) ((wood chuck))) could chuck wood))
+
+; The First Commandment (final revision)
+; When recurring on a list of atoms, lat, ask two questions about it:
+; (null? lat) and else.
+; When recurring on a number, n, ask two qusetions about it:
+; (zero? n) and else.
+; When recurring on a list of S-expressions, l, ask three questions
+; about it: (null? l), (atom? (car l)), and else.
+
+
+; The Fourth Commandment (final version)
+; Always change at least one argument while recurring.
+; When recurring on a list of atoms, lat, use (cdr lat).
+; When recurring on a number, n, use (sub1 n).  And when
+; recurring on a list of S-expressions, l, use (car l) and
+; (cdr l) if neither (null? l) nor (atom? (car l)) are true.
+;
+; It must be changed to be closer to termination.  The changing
+; argument must be tested in the termination condition.
+;
+; When using cdr, test termination with null? and
+; when using sub1, test termination with zero?.
+
+(define occur*
+  (lambda (a l)
+    (cond
+      ((null? l) 0)
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) a) (add1 (occur* a (cdr l))))
+         (else (occur* a (cdr l)))))
+      (else (+ (occur* a (car l)) (occur* a (cdr l)))))))
+
+(occur* 'banana
+        '((banana)
+         (split ((((banana ice)))
+                 (cream (banana))
+                 sherbet))
+         (banana)
+         (bread)
+         (banana brandy)))
+
+(define subst*
+  (lambda (new old l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) old) (cons new (subst* new old (cdr l))))
+         (else (cons (car l) (subst* new old (cdr l)))))
+      (else (cons (subst* new old (car l)
+       
+       
+              
+              
+      
